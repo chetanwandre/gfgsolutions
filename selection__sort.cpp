6045a7809a11283079681002765dd6d64fcc88ed
Chetan Wandre <164233886+chetanwandre@users.noreply.github.com>
2024-12-09 14:20:25 +0530

    Create selection__sort.cpp

---
+++ selection__sort.cpp
@@ -0,0 +1,39 @@
+
+
+// Given an array arr, use selection sort to sort arr[] in increasing order.
+
+// Examples :
+
+// Input: arr[] = [4, 1, 3, 9, 7]
+// Output: [1, 3, 4, 7, 9]
+// Explanation: Maintain sorted (in bold) and unsorted subarrays. Select 1. Array becomes 1 4 3 9 7. Select 3. Array becomes 1 3 4 9 7. Select 4. Array becomes 1 3 4 9 7. Select 7. Array becomes 1 3 4 7 9. Select 9. Array becomes 1 3 4 7 9.
+// Input: arr[] = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+// Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// Input: arr[] = [38, 31, 20, 14, 30]
+// Output: [14, 20, 30, 31, 38]
+// Constraints:
+// 1 ≤ arr.size() ≤ 103
+// 1 ≤ arr[i] ≤ 106
+
+
+
+class Solution {
+  public:
+    // Function to perform selection sort on the given array.
+    void selectionSort(vector<int> &arr) {
+        // code here
+        for(int i = 0; i <= arr.size()-2; i++){
+            int mini = i;
+            for(int j = i; j <= arr.size()-1; j++){
+                if(arr[j] < arr[mini]){
+                    mini = j;
+                }
+            }
+            
+            //swapping mini and current element "i"
+            int temp = arr[mini];
+            arr[mini] = arr[i];
+            arr[i] = temp;
+        }
+    }
+};
